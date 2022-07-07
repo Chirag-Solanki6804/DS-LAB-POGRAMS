d2fbbe9551b81492dcb1f34fcfc7fe48fdb56e11
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-07-07 22:40:33 +0530

    Update P10.java

--- P10.java
+++ P10.java
@@ -1,127 +1,148 @@
 /* 10. Implement a program for stack that performs following operations using array: 
 PUSH, POP, PEEP, CHANGE & DISPLAY */
-import java.util.Scanner;  
-class Stack   
-{
-    int top;   
-    int maxsize = 10;  
-    int[] arr = new int[maxsize]; 
-    Scanner sc=new Scanner(System.in); 
-      
-      
-    boolean isEmpty()  
-    {  
-        return (top < 0);  
-    }  
-    Stack()  
-    {  
-        top = -1;  
-    }  
-    boolean push (Scanner sc)  
-    {  
-        if(top == maxsize-1)  
-        {  
-            System.out.println("Overflow !!");  
-            return false;  
-        }  
-        else   
-        {  
-            System.out.println("Enter Value");  
-            int val = sc.nextInt();  
-            top++;  
-            arr[top]=val;  
-            System.out.println("Item pushed");  
-            return true;  
-        }  
-    }  
-    boolean pop ()  
-    {  
-        if (top == -1)  
-        {  
-            System.out.println("Underflow !!");  
-            return false;  
-        }  
-        else   
-        {  
-            top --;   
-            System.out.println("Item popped");  
-            return true;  
-        }  
-    } 
-
+/* 10. Implement a program for stack that performs following operations using array: 
+PUSH, POP, PEEP, CHANGE & DISPLAY */
+import java.util.Scanner;
+class stack{
+    int top;
+    int maxsize=100;
+    int[] a=new int[maxsize];
+    stack(){
+        top=-1;
+    }
+    Scanner sc=new Scanner(System.in);
+    void push(){
+        if(top>maxsize-1){
+            System.out.println("\nstack OverFlow\n");
+        }else{
+        top++;
+        System.out.println("\nEnter Number For Push:");
+        int x=sc.nextInt();
+        a[top]=x;
+        System.out.println("Number Pushed");
+        }
+    }
+    void pop(){
+         if(top<0){
+            System.out.println("\nstack UnderFlow\n");
+        }
+        else{
+        System.out.println("\n\n");
+        System.out.println(a[top]+"is Element Is Remove From The Stack\n");
+        top--;
+        System.out.println("number Poped");
+        }
+    }
     void peep(){
-       	System.out.println("Enter elements Index");
-    	int i=sc.nextInt();
-    	if(i>top||i<0){
-    		System.out.println("Index Outoff Stack");
-    	}
-    	System.out.println(arr[i]);
+         if(top<0){
+            System.out.println("\nstack UnderFlow\n");
+        }
+        else if(top>maxsize-1){
+            System.out.println("\nstack OverFlow\n");
+        }else{
+        System.out.println("\nEnter number's Index Which you want to show\n");
+        int x=sc.nextInt();
+        int b=top-x+1;
+        System.out.println("\n");
+        System.out.println(a[b]);
+        }
+    }
+    void peek(){
+         if(top<0){
+            System.out.println("\nstack UnderFlow\n");
+        }
+        else if(top>maxsize-1){
+            System.out.println("\nstack OverFlow\n");
+        }else{
+        System.out.println("\n");
+        System.out.println("\nTop Element Of stack:\n");
+        System.out.println(a[top]);
+        }
     }
     void change(){
-
+         if(top<0){
+            System.out.println("\nstack UnderFlow\n");
+        }
+        else if(top>maxsize-1){
+            System.out.println("\nstack OverFlow\n");
+        }else{
+        System.out.println("\nEnter number's Index Which you Want to change\n");
+        int x=sc.nextInt();
+        int b=top-x+1;
+        System.out.println("\nEnter Number :");
+        int d=sc.nextInt();
+        a[b]=d;
+        System.out.println("Value changed");
+        }
     }
-             
-    void display ()  
-    {  
-        System.out.println("Printing stack elements .....");  
-        for(int i = top; i>=0;i--)  
-        {  
-            System.out.println(arr[i]);  
-        }  
+    void display(){
+         if(top<0){
+            System.out.println("\nstack Empty\n");
+        }
+        System.out.println("\nYour Stack\n");
+        for(int i=top;i>=0;i--){
+            System.out.println("\n");
+            System.out.println(a[i]);
+        }
     }
-
-}  
-public class P10 {  //P10 means Stack_Operations 
-public static void main(String[] args) {  
-    int choice=0;  
-    Scanner sc = new Scanner(System.in);  
-    Stack s = new Stack();  
-    System.out.println("*********Stack operations using array*********\n");  
-    System.out.println("\n------------------------------------------------\n");  
-    while(choice != 6)  
-    {  
-        System.out.println("\nChose one from the below options...\n");  
-        System.out.println("\n1.Push\n2.Pop\n3.Peep\n4.Change\n5.display\n6.Exit");  
-        System.out.println("\n Enter your choice \n");        
-        choice = sc.nextInt();  
-        switch(choice)  
-        {  
-            case 1:  
-            {   
-                s.push(sc);  
-                break;  
-            }  
-            case 2:  
-            {  
-                s.pop();  
-                break;  
-            } 
+    void stop(){
+        System.out.println("\n\n\nprogarm End\n\n\n");
+        System.exit(0);
+    }
+}
+public class P10{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=0;
+        stack s=new stack();
+            while(n!=6){
+            System.out.println("****************STACK OPRATIONS******************");
+            System.out.println("\n\n\n");
+            System.out.println("---------------------------------------------");
+            System.out.println("\nEnter below Number to Perfrom stack oprations\n");
+            System.out.println("1.PUSH\n\n2.POP\n\n3.PEEP\n\n4.PEEK\n\n5.CHANGE\n\n6.DISPLAY\n\n7.STOP\n\n");
+            n=sc.nextInt();
+            switch(n){
+            case 1:
+                {
+                    s.push();
+                    break;
+                }
+            case 2:
+                {
+                    s.pop();
+                    break;
+                }
             case 3:
-            {
-            	s.peep();
-            	break;
-            }
+                {
+                    s.peep();
+                    break;
+                }
             case 4:
-            {
-            	s.change();
-            	break;
+                {
+                    s.peek();
+                    break;
+                }
+            case 5:
+                {
+                   s.change();
+                   break;
+                }
+            case 6:
+                {
+                    s.display();
+                    break;
+                }
+            case 7:
+                {
+                    s.stop();
+                    break;
+                }
+            default:
+                System.out.println("Enter valid Input");
+
             }
-            case 5:  
-            {  
-                s.display();  
-                break;  
-            }  
-            case 6:   
-            {  
-                System.out.println("Exiting....");  
-                System.exit(0);  
-                break;   
-            }  
-            default:  
-            {  
-                System.out.println("Please Enter valid choice ");  
-            }   
-        };  
-    }  
-}  
-}  
+        }
+    }
+} 
+ 
