8892872c4386701d438a0921a745c3ed1bc29848
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-07-18 23:44:40 +0530

    Update P12.java
    program to convert in-fix notation to post-fix notation using stack

--- P12.java
+++ P12.java
@@ -1,92 +1,79 @@
 // 12. Implement a program to convert in-fix notation to post-fix notation using stack
-import java.io.*;  
-class Stack  
-{  
-char a[]=new char[100];  
-int top=-1;  
-void push(char c)  
-{  
-try  
-{  
-a[++top]= c;  
-}  
-catch(StringIndexOutOfBoundsException e)  
-{  
-System.out.println("Stack full, no room to push, size=100");  
-System.exit(0);  
-}  
-}  
-char pop()  
-{  
-return a[top--];  
-}  
-boolean isEmpty()  
-{  
-return (top==-1)?true:false;  
-}  
-char peek()  
-{  
-return a[top];  
-}  
-}     
-public class InfixToPostfix  
-{  
-static Stack operators = new Stack();         
-public static void main(String argv[]) throws IOException  
-{  
-String infix;  
-//create an input stream object  
-BufferedReader keyboard = new BufferedReader (new InputStreamReader(System.in));  
-//get input from user  
-System.out.print("\nEnter the infix expression you want to convert: ");  
-infix = keyboard.readLine();  
-//output as postfix  
-System.out.println("Postfix expression for the given infix expression is:" + toPostfix(infix));  
-}  
-private static String toPostfix(String infix)  
-//converts an infix expression to postfix  
-{  
-char symbol;  
-String postfix = "";  
-for(int i=0;i<infix.length();++i)  
-//while there is input to be read  
-{  
-symbol = infix.charAt(i);  
-//if it's an operand, add it to the string  
-if (Character.isLetter(symbol))  
-postfix = postfix + symbol;  
-else if (symbol=='(')  
-//push (  
-{  
-operators.push(symbol);  
-}  
-else if (symbol==')')  
-//push everything back to (  
-{  
-while (operators.peek() != '(')  
-{  
-postfix = postfix + operators.pop();  
-}  
-operators.pop();        //remove '('  
-}  
-else  
-//print operators occurring before it that have greater precedence  
-{  
-while (!operators.isEmpty() && !(operators.peek()=='(') && prec(symbol) <= prec(operators.peek()))  
-postfix = postfix + operators.pop();  
-operators.push(symbol);  
-}  
-}  
-while (!operators.isEmpty())  
-postfix = postfix + operators.pop();  
-return postfix;  
-}  
-static int prec(char x)  
-{  
-if (x == '+' || x == '-')  
-return 1;  
-if (x == '*' || x == '/' || x == '%')  
-return 2;  
-return 0;  
-}  
-}  
+import java.util.*;
+import java.io.*;
+public class P12 {
+    static P12 i1 = new P12();
+    public static void main(String[] args) {
+        System.out.println("Enter a Infix String : ");
+        Scanner sc = new Scanner(System.in);
+        String infix = sc.nextLine();
+        System.out.println("PostFix Expression : " + i1.ItoP(infix));
+    }
+
+    char[] arr1 = new char[100];
+    static int top = -1;
+
+    public void push(char n) {
+        if (top >= 100) {
+            System.out.println("STACK OVERFLOW");
+        }
+        top++;
+        arr1[top]=n;
+    }
+
+    public char pop() {
+        if (top == -1) {
+            System.out.println("STACK UNDERFLOW");
+            return 0;
+        }
+        return arr1[top--];
+    }
+
+    public char peek() {
+        if (top <= -1) {
+            System.out.println("STACK UNDERFLOW");
+            return 0;
+        }
+        return arr1[top];
+    }
+
+    public int SetPrecedenc(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return ch;
+    }
+
+    public String ItoP(String infix) {
+        char symobl;
+        String PostFix = "";
+        for (int i = 0; i < infix.length();i++) {
+            symobl = infix.charAt(i);
+            if (Character.isLetter(symobl)) {
+                PostFix += symobl;
+            } else if (symobl == '(') {
+                i1.push(symobl);
+            } else if (symobl == ')') {
+                while (i1.peek() != '(') {
+                    PostFix += pop();
+                }
+                i1.pop();
+            } else {
+                while (top != -1 && !(i1.peek() == '(') && SetPrecedenc(symobl) <= SetPrecedenc(i1.peek())) 
+                    PostFix += i1.pop();
+                    i1.push(symobl);
+                }
+            }        
+        while (top != -1) {
+            PostFix += i1.pop();
+        }
+        return PostFix;
+    }
+}
